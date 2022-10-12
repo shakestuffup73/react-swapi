d356eee7e1a5379c76ce7efcc9a78c5ce287127a
Amanda Stern <amanda.stern.73@gmail.com>
2022-10-12 17:54:54 -0400

    map starships

---
+++ src/pages/Starships/Starships.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react"
+import { getAllStarships } from "../../services/sw-api";
+import { Link } from "react-router-dom"
+
+
+const Starships = () => {
+
+  const [starships, setStarships] = useState({})
+
+  useEffect(() => {
+    const fetchStarshipData = async () => {
+      const starshipData = await getAllStarships()
+      setStarships(starshipData.results)
+      // console.log(starshipData)
+    }
+    fetchStarshipData()
+  }, []) 
+
+  return ( 
+    <>
+      <h1>This is the Starships Component</h1>
+      {starships.length ?
+      <>
+        {starships.map(starship => 
+          <div key={starship.model}>
+            <Link to="/" state={{starship}}>{starship.name}</Link>
+            <br />
+          </div>  
+        )}
+      </>
+      :
+      <>
+        <h3>Loading Starships...</h3>
+      </>
+      }
+    </>
+  );
+}
+
+export default Starships;
